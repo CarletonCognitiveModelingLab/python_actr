ea8bfc0504fab34f8e8153f79b68d969bf77211d
Terry Stewart <tcstewar@uwaterloo.ca>
2021-12-21 06:37:41 -0500

    Started version 1.9.2

--- python_actr/version.py
+++ python_actr/version.py
@@ -1,6 +1,6 @@
 name = "python_actr"
-version_info = (1, 9, 1)
-dev = False
+version_info = (1, 9, 2)
+dev = True
 version = '.'.join([str(x) for x in version_info])
 if dev:
     version = version + 'dev'
