807d733ced73b367fa5562343e5f20b767ec97a3
Ahmad Sohrabi <123218209+ASohraB@users.noreply.github.com>
2023-10-20 10:34:33 -0400

    Update csdh.py

--- python_actr/csdh.py
+++ python_actr/csdh.py
@@ -1,16 +1,16 @@
-#!pip install python_actr #uncomment this if needed
-#from python_actr import *
-class MyEnv(Model):
-  pass
-class MyAgent(ACTR):
-  goal = Buffer() # Creating the goal buffer for the agent
-  def init(): # this rule fires when the agent is instantiated.
-    goal.set("helloworld") # set goal buffer to direct program flow
-  def bread_bottom(goal="helloworld"): # if goal="helloworld" , fire rule
-    print ("Hello World!")
-    goal.set("stop") # set goal buffer to direct program flow
-  #def stop_production(goal="stop"):
-    #self.stop() # stop the agent
+##!pip install python_actr #uncomment this if needed
+##from python_actr import *
+#class MyEnv(Model):
+#  pass
+#class MyAgent(ACTR):
+#  goal = Buffer() # Creating the goal buffer for the agent
+#  def init(): # this rule fires when the agent is instantiated.
+#    goal.set("helloworld") # set goal buffer to direct program flow
+#  def bread_bottom(goal="helloworld"): # if goal="helloworld" , fire rule
+#    print ("Hello World!")
+#    goal.set("stop") # set goal buffer to direct program flow
+#  #def stop_production(goal="stop"):
+#    #self.stop() # stop the agent
     
 celscilist = {
 'dd':{'x':{'followers': 400}},
@@ -35,8 +35,8 @@ for person in celscilist:
     #dh=ic(person,checkfame(person))
     print(person,checkfame(person))
         
-tim = MyAgent()
-subway=MyEnv()
-subway.agent=tim
-log_everything(subway)
-subway.run()
+#tim = MyAgent()
+#subway=MyEnv()
+#subway.agent=tim
+#log_everything(subway)
+#subway.run()
